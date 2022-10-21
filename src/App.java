dc9b44bad2d3061ba226bf5c7af1813df5e16a52
JerryChen <xiaoming_0930@foxmial.com>
2022-10-21 21:29:25 +0800

    修改：     bin/App.class 	修改：     src/App.java

--- src/App.java
+++ src/App.java
@@ -35,6 +35,7 @@ public class App {
                 Flag = input("Exit(1/0)") == 0;
                 map.setUpMap();
                 Iswin = map.isWin();
+                continue ;
             }
 
             map.mapOut();
@@ -47,6 +48,7 @@ public class App {
                 Flag = input("Exit(1/0)") == 0;
                 map.setUpMap();
                 Iswin = map.isWin();
+                continue ;
             }
 
         }
